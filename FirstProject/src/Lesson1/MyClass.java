0841c5f5bf87c3e37c2bf59bc3ca88678c403492
lidiabernabe <lidiabernabe@localhost>
2021-08-07 23:36:36 -0400

    first commit

---
+++ FirstProject/src/Lesson1/MyClass.java
@@ -0,0 +1,33 @@
+package Lesson1;
+
+public class MyClass {
+
+
+	/*byte (number,1)
+	 *short ( number,2 bytes)
+	 * int ( number,4 bytes)
+	 * long ( number,8 bytes )
+	 * float (float number,4 numbers)
+	 * double (float number,8 numbers)
+	 * char (a character, 2 bytes)
+	 * boolean (true or false, 1 byte)
+	 */
+
+	public static void main(String[] args) {
+
+		short my_variable = 10;
+		int my_number = 1234;
+		float my_decimal = (float)4.5;
+		double my_double = 11.56;
+		char my_char = 'A';
+		boolean is_true = false;
+		System.out.println(my_variable);
+		System.out.println(my_number);
+		System.out.println(my_decimal);
+		System.out.println(my_double);
+		System.out.println(my_char);
+		System.out.println(is_true);
+
+	}
+
+}
